a86cded5834c0adadbb9225c4d0f6e804c4fa04d
Moathal <moathal.s.kachi@gmail.com>
2022-09-17 09:40:58 +0300

    Solved

---
+++ anagram-checker.js
@@ -0,0 +1,39 @@
+/*  module.exports = function (text, str) {
+ 
+  text = text.replace(/\s|\|&;\$%@"<>\(\)\+|\,/g, "");
+  text = text.toLowerCase();
+  str = str.replace(/\s|\|&;\$%@"<>\(\)\+|\,/g, "");
+  str = str.toLowerCase();
+  if (str.length !== text.length) return false;
+  for (let i = 0; i < text.length; i++) {
+    if (!str.includes(text[i])) return false;
+    for (let j = 0; j <= str.length; j++) {
+      if (str[j] == text[i]) {
+        str = str.replace(str[j],"");
+        break;
+      }
+    }
+  }
+  return true;
+}; */
+
+function test(text, str) {
+ 
+  text = text.replace(/\s|\|&;\$%@"<>\(\)\+|\,/g, "");
+  text = text.toLowerCase();
+  str = str.replace(/\s|\|&;\$%@"<>\(\)\+|\,/g, "");
+  str = str.toLowerCase();
+  if (str.length !== text.length) return false;
+  for (let i = 0; i < text.length; i++) {
+    if (!str.includes(text[i])) return false;
+    for (let j = 0; j <= str.length; j++) {
+      if (str[j] == text[i]) {
+        str = str.replace(str[j],"");
+        break;
+      }
+    }
+  }
+  return true;
+};
+
+test ("butt", "buut");
